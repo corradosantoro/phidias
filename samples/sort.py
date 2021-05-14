c87898fe1577dfa3435468d4d3eb13e8cc6e9f4c
corrado <santoro@dmi.unict.it>
2021-05-14 09:36:19 +0200

    modifiche

--- samples/sort.py
+++ samples/sort.py
@@ -3,9 +3,9 @@
 #
 
 import sys
-import random
-
 sys.path.insert(0, "../lib")
+
+import random
 
 from phidias.Types import *
 from phidias.Main import *
@@ -18,8 +18,9 @@ class dump(Procedure): pass
 
 
 class RandomUniform(Action):
-    def execute(self, size, x):
-        x(random.uniform(0, size()))
+    def execute(self, _max, x):
+        value = random.uniform(0, _max())
+        x(value)
 
 
 def_vars('N', 'V', 'N1', 'N2', 'V1', 'V2', 'Sorted')
@@ -35,13 +36,13 @@ sort(N1,N2,Sorted) / (item(N1, V1) & item(N2, V2) & gt(V1,V2)) >> \
   [
       -item(N1, V1), -item(N2, V2),
       +item(N1, V2), +item(N2, V1),
-      "N1 = N1+1", "N2=N2+1",
-      sort(N1,N2,False)
+      "N1 = N1 + 1", "N2 = N2 + 1",
+      sort(N1, N2, False)
   ]
 sort(N1,N2,Sorted) / (item(N1, V1) & item(N2, V2)) >> \
   [
-      "N1 = N1+1", "N2=N2+1",
-      sort(N1,N2,Sorted)
+      "N1 = N1 + 1", "N2 = N2 + 1",
+      sort(N1, N2, Sorted)
   ]
 sort(N1,N2,False)  >> [ sort(1,2,True) ]
 sort(N1,N2,True)  >> [ show_line("Completed") ]
@@ -51,3 +52,4 @@ dump(N) / item(N, V) >> [ show_line(N, " --> ", V), "N = N + 1", dump(N) ]
 
 PHIDIAS.run()
 PHIDIAS.shell(globals())
+
